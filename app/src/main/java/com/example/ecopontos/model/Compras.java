801ca86b3b2b5e12b765bb85844b3b65d94497d7
Kauan-Correa0 <kauanschott121@gmail.com>
2026-05-18 16:57:27 -0300

    Model de Compras criado

--- app/src/main/java/com/example/ecopontos/model/Compras.java
+++ app/src/main/java/com/example/ecopontos/model/Compras.java
@@ -1,4 +1,43 @@
 package com.example.ecopontos.model;
 
-public class Compras {
+import java.io.Serializable;
+
+public class Compras implements Serializable {
+    String id_compra;
+    String id_promocao;
+    String id_usuario;
+    public Compras(String id_compra, String id_promocao, String id_usuario) {
+        this.id_compra = id_compra;
+        this.id_promocao = id_promocao;
+        this.id_usuario = id_usuario;
+    }
+
+    public Compras(String id_usuario, String id_promocao) {
+        this.id_usuario = id_usuario;
+        this.id_promocao = id_promocao;
+    }
+
+    public String getId_compra() {
+        return id_compra;
+    }
+
+    public void setId_compra(String id_compra) {
+        this.id_compra = id_compra;
+    }
+
+    public String getId_promocao() {
+        return id_promocao;
+    }
+
+    public void setId_promocao(String id_promocao) {
+        this.id_promocao = id_promocao;
+    }
+
+    public String getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(String id_usuario) {
+        this.id_usuario = id_usuario;
+    }
 }
