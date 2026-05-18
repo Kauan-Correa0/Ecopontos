1d7691b6bee7e3d4d2878dde5ab88b48b7179ad8
Kauan-Correa0 <kauanschott121@gmail.com>
2026-05-18 16:52:42 -0300

    Model de parceiros criado

--- app/src/main/java/com/example/ecopontos/model/Parceiros.java
+++ app/src/main/java/com/example/ecopontos/model/Parceiros.java
@@ -1,4 +1,44 @@
 package com.example.ecopontos.model;
 
-public class Parceiros {
+import java.io.Serializable;
+
+public class Parceiros implements Serializable {
+    String id_parceiro;
+    String nome;
+    String categoria;
+
+    public Parceiros(String id_parceiro, String nome, String categoria) {
+        this.id_parceiro = id_parceiro;
+        this.nome = nome;
+        this.categoria = categoria;
+    }
+
+    public Parceiros(String nome, String categoria) {
+        this.nome = nome;
+        this.categoria = categoria;
+    }
+
+    public String getId_parceiro() {
+        return id_parceiro;
+    }
+
+    public void setId_parceiro(String id_parceiro) {
+        this.id_parceiro = id_parceiro;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 }
